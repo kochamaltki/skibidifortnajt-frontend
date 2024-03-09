a61976fd52834f0d05c899aa150b120ec123ef1f
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-03-09 23:19:18 +0100

    added test for the Profile component

--- src/lib/components/Profile/Profile.test.ts
+++ src/lib/components/Profile/Profile.test.ts
@@ -1,11 +1,16 @@
-import { afterEach, describe, it } from 'vitest'
+import { afterEach, describe, expect, it } from 'vitest'
 import { cleanup, render } from '@testing-library/svelte'
 import Profile from './Profile.svelte'
 
 describe('Profile.svelte', () => {
 	afterEach(() => cleanup())
 
+	let name: string = "Test Name";
 	it('mounts', () => {
-		render(Profile)
+		const { container } = render(Profile, {
+			displayName: name
+		});
+		expect(container).toBeTruthy();
+		expect(container.innerHTML).toContain(name);
 	})
 })
