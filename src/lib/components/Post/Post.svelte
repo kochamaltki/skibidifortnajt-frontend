a7fab0777fc36735a39f6c5155611e35759318d3
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-03-10 14:44:09 +0100

    post component prototype

--- src/lib/components/Post/Post.svelte
+++ src/lib/components/Post/Post.svelte
@@ -3,7 +3,7 @@
 
 	export let displayName: string = "displayName";
 	export let username: string = "username";
-	export let content: string = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.";
+	export let content: string = "ALEKSANDER KLIK"
 
 	export let timePosted: number = 0;
 
@@ -14,8 +14,113 @@
 	}
 </script>
 
+<div class="container">
+	<div class="left">
+		<img class="profilepicture" src="{profilePictureUrl}" alt="profilepicture">
+		<div class="divider"></div>
+	</div>
+	<div class="content">
+		<div class="header">
+			<div class="profile">
+				<div class="names">
+					<h1>{displayName}</h1>
+					<p>@{username}</p>
+				</div>
+			</div>
+			<p>
+				{format(timePosted)}
+			</p>
+		</div>
 
+		<div class="postcontent">
+			<h2>
+				{content}
+			</h2>
+		</div>
+	</div>
+</div>
 
 <style lang="scss">
+	$profilePictureWidth: 4rem;
+	$lightGray: #949494;
 
+	.left {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		row-gap: 2rem;
+	}
+
+	.container {
+		background-color: #313131;
+		padding: 2rem;
+		border-radius: 30px;
+
+		display: flex;
+		column-gap: 0rem;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		row-gap: 1em;
+	}
+
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-start;
+		margin-left: 1rem;
+	}
+
+	p {
+		margin: 0px;
+
+		color: $lightGray;
+		font-size: 1.25rem;
+	}
+
+	h1 {
+		margin: 0px;
+
+		color: white;
+		font-weight: normal;
+		font-size: 2rem;
+	}
+
+	h2 {
+		margin: 0px;
+		
+		font-weight: normal;
+		font-size: 1.25rem;
+	}
+
+
+	.profile {
+		display: flex;
+		column-gap: 1em;
+	}
+
+	.profilepicture {
+		max-width: $profilePictureWidth;
+		border-radius: $profilePictureWidth;
+	}
+
+	.names {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.postcontent {
+		color: white;
+		padding: .5em;
+	}
+
+	.divider {
+		width: 2px;
+		height: 100%;
+
+		background-color: $lightGray;
+	}
 </style>
