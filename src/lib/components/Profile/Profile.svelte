ae23f361d20d1cc249c3e4b9a4204712d89fb8c1
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-03-10 11:51:17 +0100

    replaced most px with rem inthe Profile component

--- src/lib/components/Profile/Profile.svelte
+++ src/lib/components/Profile/Profile.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 
 export let displayName: string = "displayName";
-export let username: string = "username"
-export let desc: string = "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+export let username: string = "username";
+export let desc: string = "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.";
 
-export let backgroundUrl: string = "/images/background.jpg"
-export let profilePictureUrl: string = "/images/profilepicture.jpg"
+export let backgroundUrl: string = "/images/background.jpg";
+export let profilePictureUrl: string = "/images/profilepicture.jpg";
+
+export let myProfile: boolean = true;
 
 </script>
 
@@ -17,21 +19,23 @@ export let profilePictureUrl: string = "/images/profilepicture.jpg"
 			<h2>@{username}</h2>
 		</div>
 		<p>{desc}</p>
-		<button>My Profile</button>
+		{#if myProfile}
+			<button>My Profile</button>
+		{/if}
 	</div>
 	<img class="backgroundimage" src="{backgroundUrl}" alt="background">
 </div>
 
 <style lang="scss">
 
-$maxWidth: 320px;
-$profilePictureWidth: 90px;
+$maxWidth: 20rem;
+$profilePictureWidth: 6rem;
 
 $backgroundColor: #313131;
 $lightGray: #949494;
 
 .container {
-	padding-top: 50px;
+	padding-top: 3.25rem;
 	position: relative;
 	display: flex;
 	flex-direction: column;
@@ -50,19 +54,19 @@ $lightGray: #949494;
 	width: inherit;
 	object-fit: cover;
 	top: 0;
-	height: 140px;
+	height: 9rem;
 
 	z-index: 0;
 	border-radius: 30px 30px 0px 0px;
 }
 
 .content {
-	padding: 32px;
+	padding: 2rem;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 
-	row-gap: 38px;
+	row-gap: 2rem;
 	z-index: 1;
 }
 
@@ -72,7 +76,7 @@ $lightGray: #949494;
 
 	border-radius: $maxWidth;
 	border-style: solid;
-	border-width: 8px;
+	border-width: 0.5rem;
 	border-color: $backgroundColor;
 }
 
@@ -81,7 +85,7 @@ h1 {
 
 	color: white;
 	font-weight: normal;
-	font-size: 32px;
+	font-size: 2rem;
 }
 
 h2 {
@@ -89,24 +93,24 @@ h2 {
 
 	color: $lightGray;
  	font-weight: normal;
-	font-size: 20px;
+	font-size: 1.25rem;
 }
 
 p {
 	margin: 0px;
 
 	color: white;
-	font-size: 20px;
+	font-size: 1.25rem;
 }
 
 button {
 	font-family: inherit;
-	font-size: 16px;
+	font-size: 1rem;
 	color: $lightGray;
 
 	background-color: #424242;
 	border: none;
-	padding: 12px 70px;
+	padding: 0.75rem 4.5rem;
 	border-radius: 10px;
 
 	transition: 0.4s;
