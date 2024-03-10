7c3bd0d1e8ae92c95f1deef753fe580da692846c
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-03-10 13:34:40 +0100

    added a date formatting test

--- src/lib/components/Post/Post.test.ts
+++ src/lib/components/Post/Post.test.ts
@@ -3,8 +3,13 @@ import { render } from '@testing-library/svelte'
 import Post from './Post.svelte'
 
 describe('Profile.svelte', () => {
+	let posted: number = new Date() - 5*60*1000;
+
 	it('renders and has proper values', () => {
-		const { container } = render(Post)
+		const { container } = render(Post, {
+			timePosted: posted
+		})
 		expect(container).toBeTruthy();
+		expect(container.innerHTML).toContain("5 minutes ago")
 	})
 })
