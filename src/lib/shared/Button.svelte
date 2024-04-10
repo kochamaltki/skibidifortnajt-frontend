0cd1bfc4be383b52f38e91f2114056b58a0aa1f3
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-04-10 20:51:01 +0200

    added a button component with 2 styles to choose from

--- src/lib/shared/Button.svelte
+++ src/lib/shared/Button.svelte
@@ -1,12 +1,36 @@
 <script lang="ts">
+	export let style: string = "accent";
 </script>
 
-<button class="container" on:click>
+<button class="container {style}" on:click>
 	<slot/>
 </button>
 
 <style lang="scss">
 	.container {
+		margin: 0;
+		padding: 15px 30px;
+		border: none;
+		border-radius: 50px;
 
+		font-size: 20px;
+		font-weight: bold;
+
+		cursor: pointer;
+	}
+
+	.accent {
+		color: #f0f0f0;
+		background-color: #ff4655;
+
+		transition: .3s;
+		&:hover {
+			filter: brightness(88%);
+		}
+	}
+
+	.background {
+		color: #f0f0f0;
+		background-color: #111215;
 	}
 </style>
