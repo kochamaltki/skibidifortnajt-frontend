c2f8fbdf8e6ffc62dc8864f76873917105c47e1b
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-04-11 21:46:11 +0200

    roughly styled

--- src/lib/components/LogIn.svelte
+++ src/lib/components/LogIn.svelte
@@ -9,28 +9,43 @@
 </script>
 
 <Modal bind:showModal={showModal}>
-	<div class="container">
-		<div class="top">
-			<h1>Log In</h1>
-			<Clickable on:click={() => { showModal = false }}>
-				<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="times"><path fill="#474c63" d="M13.41,12l4.3-4.29a1,1,0,1,0-1.42-1.42L12,10.59,7.71,6.29A1,1,0,0,0,6.29,7.71L10.59,12l-4.3,4.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></svg>
-			</Clickable>
+	<form on:submit={(e) => alert(e)}>
+		<div class="container">
+			<div class="top">
+				<h1>Log In</h1>
+				<Clickable on:click={() => { showModal = false }}>
+					<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="times"><path fill="#474c63" d="M13.41,12l4.3-4.29a1,1,0,1,0-1.42-1.42L12,10.59,7.71,6.29A1,1,0,0,0,6.29,7.71L10.59,12l-4.3,4.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></svg>
+				</Clickable>
+			</div>
+			<!-- input fields here -->
+			<div class="input">
+				<input type="text" placeholder="Username">
+				<input type={showPassword ? "text" : "password"} placeholder="Password">
+				<p>Don't have an account? <a href="#">Sign up</a></p>
+			</div>
+			<Button big={true} disabled={!hasValidInput} style={hasValidInput ? "accent" : "background"}>
+				Continue
+			</Button>
 		</div>
-		<!-- input fields here -->
-		<div class="input">
-			<input type="text">
-			<input type={showPassword ? "text" : "password"}>
-			<p>Don't have an account? <a href="#">Sign up<a></p>
-		</div>
-		<Button big={true} disabled={!hasValidInput} style={hasValidInput ? "accent" : "background"}>
-			Continue
-		</Button>
-	</div>
+	</form>
 </Modal>
 
 <style lang="scss">
 	input {
-		width: 100%;
+		width: 500px - 100px;
+		outline: none;
+		border: none;
+		border-radius: 100px;
+
+		font-size: 24px;
+		padding: 15px 30px;
+
+		background-color: #111215;
+		color: #f0f0f0;
+		
+		&::placeholder {
+			color: #181b29;
+		}
 	}
 
 	a {
@@ -53,7 +68,7 @@
 	}
 
 	.container {
-		width: 800px - 40px;
+		width: 500px - 40px;
 		display: flex;
 		flex-direction: column;
 		gap: 60px;
