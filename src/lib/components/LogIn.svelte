55970699c7279b345abd91a77b7f32126dafa902
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-04-14 15:54:54 +0200

    log in with backend backup

--- src/lib/components/LogIn.svelte
+++ src/lib/components/LogIn.svelte
@@ -2,24 +2,45 @@
 	import Modal from "$lib/shared/Modal.svelte";
 	import Button from "$lib/shared/Button.svelte";
 
+	export let apiUrl: string = "http://localhost:8000";
+
 	let showModal: boolean = true;
 	let hasValidInput: boolean = true;
-	let showPassword: boolean = false;
+
+	let usernameInput: string = ""
+	let passwordInput: string = ""
+
+	const logIn = async (username: string, password: string) => {
+		fetch(apiUrl + "/api/post/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				user_name: username,
+				passwd: password
+			})
+		}).then(res => {
+			return res.json()
+		}).then(data => {
+			console.log(data)
+		});
+	}
 </script>
 
 <Modal bind:showModal={showModal}>
-	<form on:submit={(e) => alert("submit")}>
+	<form on:submit={() => logIn(usernameInput, passwordInput)}>
 		<div class="container">
 			<h1>Log In</h1>
 
 			<div class="input">
 				<div class="input-box">
-					<input type="text" placeholder="Username" required>
-					<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="user"><path fill="#181b29" d="M15.71,12.71a6,6,0,1,0-7.42,0,10,10,0,0,0-6.22,8.18,1,1,0,0,0,2,.22,8,8,0,0,1,15.9,0,1,1,0,0,0,1,.89h.11a1,1,0,0,0,.88-1.1A10,10,0,0,0,15.71,12.71ZM12,12a4,4,0,1,1,4-4A4,4,0,0,1,12,12Z"></path></svg>
+					<input bind:value={usernameInput} type="text" placeholder="Username" required>
+					<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="user"><path fill="#313646" d="M15.71,12.71a6,6,0,1,0-7.42,0,10,10,0,0,0-6.22,8.18,1,1,0,0,0,2,.22,8,8,0,0,1,15.9,0,1,1,0,0,0,1,.89h.11a1,1,0,0,0,.88-1.1A10,10,0,0,0,15.71,12.71ZM12,12a4,4,0,1,1,4-4A4,4,0,0,1,12,12Z"></path></svg>
 				</div>
 				<div class="input-box">
-					<input type={showPassword ? "text" : "password"} placeholder="Password" required>
-					<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="lock"><path fill="#181b29" d="M17,9V7A5,5,0,0,0,7,7V9a3,3,0,0,0-3,3v7a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V12A3,3,0,0,0,17,9ZM9,7a3,3,0,0,1,6,0V9H9Zm9,12a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V12a1,1,0,0,1,1-1H17a1,1,0,0,1,1,1Z"></path></svg>
+					<input bind:value={passwordInput} type="password" placeholder="Password" required>
+					<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="lock"><path fill="#313646" d="M17,9V7A5,5,0,0,0,7,7V9a3,3,0,0,0-3,3v7a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V12A3,3,0,0,0,17,9ZM9,7a3,3,0,0,1,6,0V9H9Zm9,12a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V12a1,1,0,0,1,1-1H17a1,1,0,0,1,1,1Z"></path></svg>
 				</div>
 			</div>
 
@@ -46,7 +67,7 @@
 		color: #f0f0f0;
 		
 		&::placeholder {
-			color: #181b29;
+			color: #313646;
 		}
 	}
 
