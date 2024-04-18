062c42f9d5e790a930f77d0d145eceb84d946d48
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-04-18 20:00:10 +0200

    added the bar to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
 	import '@fontsource-variable/outfit'
+	import LogIn from "$components/LogIn.svelte";
+	import TopBar from '$components/TopBar.svelte';
+
+	import apiUrl from "../stores/apiUrl";
 </script>
 
 <div class="container">
+	<header>
+		<TopBar/>
+	</header>
 	<slot/>
+	<LogIn {apiUrl}/>
 </div>
 
 <style lang="scss">
+	header {
+		position: sticky;
+		top: 0;
+		width: 100%;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+
+		gap: 40px;
+	}
+
 	:global(html) {
 		font-family: 'Outfit Variable', sans-serif;
 		color: #f0f0f0;
