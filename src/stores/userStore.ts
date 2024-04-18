54df79131b0d63024d46a4e976c71467f3e9b8bc
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-04-18 17:56:47 +0200

    currently logged in user is now stored in a store

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,11 +1,41 @@
 import { writable } from "svelte/store";
+import apiUrl from "./apiUrl";
 
-const userStore = writable({
-	loggedIn:  false,
-	username: "username",
-	displayName: "displayName",
-	description: "description",
-	id: -1,
-});
+export function createUserStore(apiUrl: string) {
+	const store = writable({
+		loggedIn:  false,
+		username: "username",
+		displayName: "displayName",
+		description: "description",
+		id: -1,
+	});
 
-export default userStore;
+	async function logInWithId(id: number) {
+		let res = await fetch(apiUrl + "/api/get/profile/by-id/" + id);
+		let data: any = await res.json();
+
+		store.set({
+			loggedIn: true,
+			username: data.user_name,
+			displayName: data.display_name,
+			description: data.description,
+			id: data.id
+		});
+	}
+
+	async function logInWithUsername(username: string) {
+		let res = await fetch(apiUrl + "/api/get/user/id/" + username);
+		let id = await res.json();
+			
+		return logInWithId(id);
+	}
+
+	return {
+		...store,
+		logInWithId,
+		logInWithUsername
+	};
+}
+
+
+export const userStore = createUserStore(apiUrl);
