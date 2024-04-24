ce7d27dc515a43f036efa779bf7f586b72a3f103
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-04-24 21:01:23 +0200

    like count formatting (e.g. 1000 = 1K)

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -10,6 +10,8 @@
 	import apiUrl from '$stores/apiUrl';
 	import { userStore } from '$stores/userStore';
 
+	const formatter = Intl.NumberFormat("en", { notation: "compact" })
+
 	const getRandomProiflePicture = () => {
 		let index: number = Math.floor(Math.random() * 10);
 		return `/images/altki/altka${index}.png`;
@@ -25,7 +27,11 @@
 	export let postId: number = -1;
 
 	export let liked: boolean = false;
+
 	let endpoint: string;
+	let likes: string;
+
+	$: likes = formatter.format(likeCount);
 
 	const checkLiked = async () => {
 		if($userStore.loggedIn) {
@@ -112,7 +118,7 @@
 						{:else}
 							<svg in:scale={{ duration: 1500, easing: elasticOut }} class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="heart"><path fill="#f0f0f0" d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"></path></svg>
 						{/if}
-						{likeCount}
+						{likes}
 					</div>
 				</Clickable>
 				<Clickable>
