a9c798b7028e80b359d7d5d9fa463bc410a8d67b
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-05-18 17:17:06 +0200

    created a component to hold images

---
+++ src/lib/shared/ImageContainer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let images: Array<string> = [];
+	let currentIndex: number = 0;
+
+	$: arrLength = images.length;
+	$: showRight = currentIndex < arrLength - 1;
+	$: showLeft = currentIndex > 0;
+</script>
+	
+{#if arrLength}
+	<img src="{images[currentIndex]}" alt="post content"/>
+{/if}
+
+{showLeft}, {showRight}
+
+<style lang="scss">
+</style>
