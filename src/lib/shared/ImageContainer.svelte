19fe29eab4166a6b93c915adb321f875ac8763fb
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-05-18 17:31:36 +0200

    image container displays the first image from the input array and aligns it properly on all axes

--- src/lib/shared/ImageContainer.svelte
+++ src/lib/shared/ImageContainer.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
 	export let images: Array<string> = [];
-	let currentIndex: number = 0;
+	let currentIndex: number = 9;
 
 	$: arrLength = images.length;
-	$: showRight = currentIndex < arrLength - 1;
+	$: showRight = currentIndex < (arrLength - 1);
 	$: showLeft = currentIndex > 0;
+
+	onMount(() => {
+		for(let i = 0; i < 10; i+=1) {
+			images = [...images, `/images/altki/altka${i}.png`]
+		}
+	})
 </script>
 	
 {#if arrLength}
-	<img src="{images[currentIndex]}" alt="post content"/>
+	<div class="container">
+		<img src="{images[currentIndex]}" alt="post content"/>
+	</div>
 {/if}
 
-{showLeft}, {showRight}
-
 <style lang="scss">
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
