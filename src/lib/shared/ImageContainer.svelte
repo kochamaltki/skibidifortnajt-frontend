9332ea8805ea8eb28effa3e76d56f281bff30e6b
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-05-19 12:13:18 +0200

    can switch image with buttons

--- src/lib/shared/ImageContainer.svelte
+++ src/lib/shared/ImageContainer.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-    import Checkbox from "./Checkbox.svelte";
-import Clickable from "./Clickable.svelte";
+	import Clickable from "./Clickable.svelte";
 	import { onMount } from "svelte";
 
 	export let images: Array<string> = [];
-	let currentIndex: number = 9;
+	let currentIndex: number = 0;
 
 	$: arrLength = images.length;
 	$: showRight = currentIndex < (arrLength - 1);
@@ -19,22 +18,40 @@ import Clickable from "./Clickable.svelte";
 	
 {#if arrLength}
 	<div class="container">
-		<div class="changeButton">
-			<Clickable>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="angle-right"><path fill="#f0f0f0" d="M14.83,11.29,10.59,7.05a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41L12.71,12,9.17,15.54a1,1,0,0,0,0,1.41,1,1,0,0,0,.71.29,1,1,0,0,0,.71-.29l4.24-4.24A1,1,0,0,0,14.83,11.29Z"></path></svg>
-			</Clickable>
+		<div class="buttons">
+			{#if showLeft}
+				<Clickable on:click={() => {
+					currentIndex = Math.max(0, currentIndex-1);
+				}}>
+					<div class="changeButton left">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="angle-left"><path fill="#f0f0f0" d="M11.29,12l3.54-3.54a1,1,0,0,0,0-1.41,1,1,0,0,0-1.42,0L9.17,11.29a1,1,0,0,0,0,1.42L13.41,17a1,1,0,0,0,.71.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41Z"></path></svg>
+					</div>
+				</Clickable>
+			{/if}
+			{#if showRight}
+				<Clickable on:click={() => {
+					currentIndex = Math.min(arrLength-1, currentIndex+1);
+				}}>
+					<div class="changeButton right">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="angle-right"><path fill="#f0f0f0" d="M14.83,11.29,10.59,7.05a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41L12.71,12,9.17,15.54a1,1,0,0,0,0,1.41,1,1,0,0,0,.71.29,1,1,0,0,0,.71-.29l4.24-4.24A1,1,0,0,0,14.83,11.29Z"></path></svg>
+					</div>
+				</Clickable>
+			{/if}
 		</div>
 		<img src="{images[currentIndex]}" alt="post content"/>
 	</div>
 {/if}
 
 <style lang="scss">
+	$button-size: 40px;
+
 	.changeButton {
+		margin: 0 20px;
 		background-color: #111215;
 		padding: 10px;
 		border-radius: 100%;
-		width: 40px;
-		height: 40px;
+		width: $button-size;
+		height: $button-size;
 		z-index: 2;
 
 		display: flex;
@@ -42,6 +59,15 @@ import Clickable from "./Clickable.svelte";
 		justify-content: center;
 
 		position: absolute;
+		top: calc(50% - calc(0.5 * $button-size));
+	}
+
+	.left {
+		left: 0;
+	}
+
+	.right {
+		right: 0;
 	}
 
 	img {
@@ -55,5 +81,7 @@ import Clickable from "./Clickable.svelte";
 		display: flex;
 		justify-content: center;
 		align-items: center;
+
+		position: relative;
 	}
 </style>
