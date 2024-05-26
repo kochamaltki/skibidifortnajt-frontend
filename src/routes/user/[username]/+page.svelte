df1e8593825a7f667bdc1995cb84def5634dace5
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-05-26 18:33:14 +0200

    user page displays user posts

--- src/routes/user/[username]/+page.svelte
+++ src/routes/user/[username]/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
     import type { UserPageData } from "./+page";
+	import { userStore } from "$stores/userStore";
+	import Feed from "$lib/shared/Feed.svelte";
 
     export let data: UserPageData;
 </script>
 
-username: {data.username}
+<div class="container">
+	{#await userStore.getUserId(data.username) then id}
+		<Feed feedType={`by-user/${id}`}/>
+	{/await}
+</div>
+
+<style lang="scss">
+</style>
