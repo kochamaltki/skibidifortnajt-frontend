03039ca6d9cabddfd725ff9f13f16dc12d5ef781
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-05-27 14:08:45 +0200

    settings container + todo

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
+	import Card from "$lib/shared/Card.svelte";
 </script>
 
+<!--
+TODO: 
+- profile picture
+- display name
+- description
+- delete account
+-->
+
 <div class="container">
+	<Card>
+		
+	</Card>
 </div>
 
 <style lang="scss">
+	.container {
+		width: 800px;
+	}
 </style>
